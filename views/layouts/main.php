8f4b511d35df160e28b61292aded8eade60bf47e
Htoo Khant <htookhantlin@ucsy.edu.mm>
2021-03-04 23:29:15 +0630

    change app name

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Yii::$app->name,
+        'brandLabel' => "Htoo Khant Linn",
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
