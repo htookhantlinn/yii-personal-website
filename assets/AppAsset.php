10917a8618ecd62a7b4ec71c5a73344988d9df76
Htoo Khant <htookhantlin@ucsy.edu.mm>
2021-03-05 17:33:26 +0630

    after revert

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,20 +21,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/style.css',
-        'https://fonts.googleapis.com/css?family=Montserrat:400,700',
-        'https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic',
-        'https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700',
-
     ];
     public $js = [
-        'js/script.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
-        'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js',
-        'assets/mail/jqBootstrapValidation.js',
-        'assets/mail/contact_me.js',
-        'https://use.fontawesome.com/releases/v5.15.1/js/all.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
