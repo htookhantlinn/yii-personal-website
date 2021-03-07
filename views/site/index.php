03d4e6fd9f2b3c2f978a8394b28191e4e3a7e7d4
Htoo Khant <htookhantlin@ucsy.edu.mm>
2021-03-07 20:22:00 +0630

    change bi

--- views/site/index.php
+++ views/site/index.php
@@ -59,7 +59,7 @@ use yii\helpers\Html;
                     <div align="left">
 
                         <?= Html::tag('p', Html::encode("I’m software developer and currently living in Yangon, Myanmar. I’m trying to get Bachelor
-                            of  Computer Science degree from") . Html::a('University of Computer Studies, Yangon.', ['https://www.ucsy.edu.mm'], ['style' => 'color: white'])
+                            of  Computer Science degree from  ") . Html::a('University of Computer Studies, Yangon.', ['https://www.ucsy.edu.mm'], ['style' => 'color: white'])
                             . " I am very passionate about Web Development,Mobile Development,Software Engineering and strive to better myself as a <br/>full-stack developer, and
                             the
                             development community as a whole.", ['style' => 'font-size: medium; color: white']) ?>
