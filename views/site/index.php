b740638eda42f5827f641ade2300bc1f95a48cff
Htoo Khant <htookhantlin@ucsy.edu.mm>
2021-03-12 12:56:30 +0630

    hkl.ico

--- views/site/index.php
+++ views/site/index.php
@@ -11,7 +11,6 @@ $this->title = 'Htoo Khant Linn';
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
@@ -21,6 +20,9 @@ $this->title = 'Htoo Khant Linn';
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css"/>
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet"/>
+
+    <link rel="icon" type="image/x-icon" href="assets/img/hkl.ico" />
+
 </head>
 <body id="page-top" class="bg-dark">
 
